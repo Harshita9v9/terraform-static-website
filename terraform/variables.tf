d505b003e13562aa66d9e24b8b4fb1c0fabf5c36
Harshita9v9 <77628813+Harshita9v9@users.noreply.github.com>
2025-11-22 02:35:27 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,24 @@
+variable "subscription_id" {
+  type        = string
+  description = "Azure subscription ID"
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "Azure tenant ID"
+}
+
 variable "resource_group_name" {
-  type = string
+  type        = string
+  default     = "tf-static-rg"
 }
 
 variable "location" {
-  type = string
+  type        = string
+  default     = "East US"
 }
 
 variable "storage_account_name" {
-  type = string
+  type        = string
+  default     = "harshitatfstatic01"
 }
- 
