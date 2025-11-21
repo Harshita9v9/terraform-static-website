78631eb9995744a8773528f8b02bb99d2ac4275d
Harshita9v9 <77628813+Harshita9v9@users.noreply.github.com>
2025-11-22 02:35:56 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,4 @@
- resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
@@ -9,12 +9,21 @@ resource "azurerm_storage_account" "sa" {
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+
   static_website {
     index_document = "index.html"
+    error_404_document = "index.html"
   }
+}
+
+resource "azurerm_storage_blob" "website_file" {
+  name                   = "index.html"
+  storage_account_name   = azurerm_storage_account.sa.name
+  storage_container_name = "$web"
+  type                   = "Block"
+  source                 = "../website/index.html"
 }
 
 output "static_website_url" {
   value = azurerm_storage_account.sa.primary_web_endpoint
 }
-
