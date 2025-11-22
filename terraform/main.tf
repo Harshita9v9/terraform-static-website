2e779206edf2446290c1063bce75747b82b4d576
Harshita9v9 <77628813+Harshita9v9@users.noreply.github.com>
2025-11-23 01:31:28 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,10 @@
+# Resource Group
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
 
+# Storage Account (Static Website)
 resource "azurerm_storage_account" "sa" {
   name                     = var.storage_account_name
   resource_group_name      = azurerm_resource_group.rg.name
@@ -12,18 +14,92 @@ resource "azurerm_storage_account" "sa" {
 
   static_website {
     index_document = "index.html"
-    error_404_document = "index.html"
   }
 }
 
-resource "azurerm_storage_blob" "website_file" {
+# Upload index.html
+resource "azurerm_storage_blob" "index" {
   name                   = "index.html"
   storage_account_name   = azurerm_storage_account.sa.name
   storage_container_name = "$web"
   type                   = "Block"
-  source                 = "../website/index.html"
+  content                = "<h1>Hello from Terraform Static Website</h1>"
 }
 
-output "static_website_url" {
-  value = azurerm_storage_account.sa.primary_web_endpoint
+# Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "tf-harshita-vnet"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+# Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "tf-harshita-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Public IP
+resource "azurerm_public_ip" "pip" {
+  name                = "tf-harshita-pip"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  allocation_method   = "Static"
+}
+
+# Network Interface
+resource "azurerm_network_interface" "nic" {
+  name                = "tf-harshita-nic"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
+
+# Linux VM
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = var.vm_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+
+  admin_password = "Harshita@12345"
+
+  disable_password_authentication = false
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
+
+# SQL Server
+resource "azurerm_mssql_server" "sqlserver" {
+  name                         = var.sql_server_name
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = var.location
+  administrator_login          = var.sql_admin
+  administrator_login_password = var.sql_password
+  version                      = "12.0"
+}
+
+# SQL Database
+resource "azurerm_mssql_database" "sqldb" {
+  name      = "tfharshitaDB"
+  server_id = azurerm_mssql_server.sqlserver.id
 }
